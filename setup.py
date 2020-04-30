632de9e5a59e20cbba58c33f549fbde9c7ecd9d4
matraszek <maciej.matraszek@esensei.net>
2020-05-01 00:11:45 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='img2unicode',
-      version='0.1',
-      description='Convert images to unicode based on font templates. Especially usable in terminal.',
-      url='https://github.com/matrach/img2unicode',
-      author='Maciej Matraszek',
-      author_email='matraszek.maciej@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-            'numpy',
-            'pandas',
-            'scikit-image',
-            'pillow',
-            'n2', # For FastRenderer
-      ],
-      extras_require={
-          'develop': [
+    version='0.1',
+    description='Convert images to unicode based on font templates. Especially usable in terminal.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/matrach/img2unicode',
+    author='Maciej Matraszek',
+    author_email='matraszek.maciej@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+          'numpy',
+          'pandas',
+          'scikit-image',
+          'pillow',
+          'n2', # For FastRenderer
+    ],
+    extras_require={
+        'develop': [
             'pytest',
             'pytest-cov',
             'sphinx',
             'sphinx_autodoc_typehints',
-          ]
-      },
-      zip_safe=False)
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False)
